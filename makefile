92cbe60d6ea3029295bdbb2e365bc420b7b73d22
Jeremy Warner <jeremywrnr@gmail.com>
2015-02-14 16:25:54 -0500

    shorter demo that works

--- makefile
+++ makefile
@@ -2,9 +2,13 @@ id=53ff6f066667574816342567
 src='write-demo.ino'
 out='output.bin'
 
-all: compile
+all: clean compile
 	spark flash $(id) $(out)
+
+clean:
+	find . -type f -iname "firmware*" -exec rm {} \;
 
 compile: *.ino
 	spark compile $(src)
 	mv firmware_* $(out)
+
--- simple-write.ino
+++ simple-write.ino
@@ -1,18 +1,13 @@
-
 int led = D0; // You'll need to wire an LED to this one to see it blink.
 int led2 = D7; // This one is the built-in tiny one to the right of the USB jack
 
-void swapLed();
-
 void setup() {
-    Spark.function("swap", swapLed);
     pinMode(led, OUTPUT);
     pinMode(led2, OUTPUT);
 }
 
-void loop() { }
-
-void swapLed() {
+void loop() {
+    delay(500);
     if( digitalRead(led) == HIGH ){
         digitalWrite(led, LOW);
         digitalWrite(led2, LOW);
