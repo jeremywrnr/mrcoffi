0941ff1466ddd3339ef4e6dd7849b519b3124d95
Jeremy Warner <jeremywrnr@gmail.com>
2015-02-14 21:16:17 -0500

    sort out floaters

--- brew.ino
+++ brew.ino
@@ -12,6 +12,7 @@ int changeState(String ip) {
     if( digitalRead(led) == LOW ){
         digitalWrite(led, HIGH);
         digitalWrite(led2, HIGH);
+        cups = measureCups('ip');
     } else {
         // turn the machine on
         digitalWrite(led, LOW);
@@ -23,14 +24,20 @@ int changeState(String ip) {
 // check how many cups about to brew
 int measureCups(String ip) {
 
-    // reset the cup amt.
+    // reset cups
     cups = 0;
 
     // check and sum all levels
-    for (int pin = A0; pin <= A7; ++pin) {
-        digitalRead(pin) && cups++;
-    }
+    if( digitalRead(A0) == HIGH ) cups++;
+    if( digitalRead(A1) == HIGH ) cups++;
+    if( digitalRead(A2) == HIGH ) cups++;
+    if( digitalRead(A3) == HIGH ) cups++;
+    if( digitalRead(A4) == HIGH ) cups++;
+    if( digitalRead(A5) == HIGH ) cups++;
+    if( digitalRead(A6) == HIGH ) cups++;
+    if( digitalRead(A7) == HIGH ) cups++;
 
+    // return the amount of cups
     return cups;
 
 }
@@ -45,9 +52,14 @@ void setup() {
     // pin mode definitions
     pinMode(led, OUTPUT);
     pinMode(led2, OUTPUT);
-    for (int pin = A0; pin <= A7; ++pin) {
-        pinMode(pin, INPUT);
-    }
+    pinMode(A0, INPUT);
+    pinMode(A1, INPUT);
+    pinMode(A2, INPUT);
+    pinMode(A3, INPUT);
+    pinMode(A4, INPUT);
+    pinMode(A5, INPUT);
+    pinMode(A6, INPUT);
+    pinMode(A7, INPUT);
 
     // initial conditions
     digitalWrite(led, HIGH);
