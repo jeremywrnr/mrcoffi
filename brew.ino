8119ef8e76071c83b40bba16c0917d08949907b8
Jeremy Warner <jeremywrnr@gmail.com>
2015-02-14 21:49:44 -0500

    add testing code

--- brew.ino
+++ brew.ino
@@ -12,7 +12,6 @@ int changeState(String ip) {
     if( digitalRead(led) == LOW ){
         digitalWrite(led, HIGH);
         digitalWrite(led2, HIGH);
-        cups = measureCups('ip');
     } else {
         // turn the machine on
         digitalWrite(led, LOW);
@@ -28,17 +27,33 @@ int measureCups(String ip) {
     cups = 0;
 
     // check and sum all levels
-    if( digitalRead(A0) == HIGH ) cups++;
-    if( digitalRead(A1) == HIGH ) cups++;
-    if( digitalRead(A2) == HIGH ) cups++;
-    if( digitalRead(A3) == HIGH ) cups++;
-    if( digitalRead(A4) == HIGH ) cups++;
-    if( digitalRead(A5) == HIGH ) cups++;
-    if( digitalRead(A6) == HIGH ) cups++;
-    if( digitalRead(A7) == HIGH ) cups++;
+    for( int pin = A0; A0 < A7; pin++ ){
+        cups += checkFloater(pin);
+    }
 
     // return the amount of cups
     return cups;
+
+}
+
+// check variance of polling
+int checkFloater(int pin) {
+
+    int hi = 0;
+    int lo = 0;
+    int level = 0;
+
+    for( int i = 10; i; i++ ){
+        if( digitalRead(pin) == LOW ){
+            lo++;
+        } else {
+            hi++;
+        }
+    }
+
+    // only return high if you get at least 8 high polls
+    if( hi > 8 ) level++;
+    return level;
 
 }
 
@@ -52,14 +67,9 @@ void setup() {
     // pin mode definitions
     pinMode(led, OUTPUT);
     pinMode(led2, OUTPUT);
-    pinMode(A0, INPUT);
-    pinMode(A1, INPUT);
-    pinMode(A2, INPUT);
-    pinMode(A3, INPUT);
-    pinMode(A4, INPUT);
-    pinMode(A5, INPUT);
-    pinMode(A6, INPUT);
-    pinMode(A7, INPUT);
+    for( int pin = A0; A0 < A7; pin++ ){
+        pinMode(pin, INPUT);
+    }
 
     // initial conditions
     digitalWrite(led, HIGH);
