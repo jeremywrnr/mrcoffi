94d2ce22be277fa72a32e6144564be0d143e2d53
Jeremy Warner <jeremywrnr@gmail.com>
2015-04-26 23:19:44 -0400

    refactor brew code

--- brew.ino
+++ brew.ino
@@ -1,63 +1,68 @@
 //  BREWING THE INTERNET
 //  hackcooper 2015
 
-int mrcf = D0; // this is the pin hooked up to the mr coffee heater relay
-int led2 = D7; // This one is the built-in tiny one to the right of the USB jack
+int mrcf = D0; // the pin hooked up to the mr coffee heater relay
+int led2 = D7; // the built-in tiny one to the right of the USB jack
 int cups = 0; // placeholder for cup measurement amount
 int brewing = 0; // status var for currently brewing
-int shuttingDown = 0; // status for cleaning out water bin
-int lcdStatus = 0; // holds which LCD screen status to display
-char publishString[64]; // placeholder for json output
-unsigned long tempTime = 0; // determine when to check if full
-unsigned long finishTime = 0; // determine when to timeout
-unsigned long startTime = 0; // determine when to timeout
-unsigned long lcdTime = 0; // determine when to swap LCD text
+int lcd_status = 0; // holds which LCD screen status to display
+int shutting_down = 0; // status for cleaning out water bin
+char publish_string[64]; // placeholder for json output
+unsigned long lcd_time = 0; // determine when to swap LCD text
+unsigned long temp_time = 0; // determine when to check if full
+unsigned long start_time = 0; // determine when to timeout
+unsigned long finish_time = 0; // determine when to timeout
 
-// turn the machine on or off
-int changeState(String ip) {
+// turn the machine on
+void power_on() {
+    brewing = 1;
+    digitalWrite(mrcf, HIGH);
+    digitalWrite(led2, HIGH);
+}
+
+// turn the machine off
+void power_off() {
+    brewing = 0;
+    digitalWrite(mrcf, LOW);
+    digitalWrite(led2, LOW);
+}
+
+int brew(String ip) {
 
     // turn the machine on
-    if( digitalRead(mrcf) == LOW ){
-        digitalWrite(mrcf, HIGH);
-        digitalWrite(led2, HIGH);
-        tempTime = millis();
-        finishTime = 0;
-        brewing = 1;
-        displayBrewing();
+    if( !brewing ){
+        temp_time = millis();
+        finish_time = 0;
+        display_brewing();
+        power_on();
 
         // measure & publish amount of cups to brew
-        cups = measureCups();
-        sprintf(publishString,"{\"Cups\": %d}",cups);
-        Spark.publish("Cups",publishString);
-        return 1;
-
-    } else {
-        // turn the machine off
-        digitalWrite(mrcf, LOW);
-        digitalWrite(led2, LOW);
-        brewing = 0;
+        cups = measure_cups();
+        sprintf(publish_string,"{\"Cups\": %d}",cups);
+        Spark.publish("Cups",publish_string);
         return 1;
     }
 
     // something FUNKY going on
     return 0;
+
 }
 
 // check how many cups about to brew
-int measureCups() {
+int measure_cups() {
 
     // reset cups
     cups = 0;
 
     // check and sum all levels
-    cups += checkFloater(A0);
-    cups += checkFloater(A1);
-    cups += checkFloater(A2);
-    cups += checkFloater(A3);
-    cups += checkFloater(A4);
-    cups += checkFloater(A5);
-    cups += checkFloater(A6);
-    cups += checkFloater(A7);
+    cups += check_level(A0);
+    cups += check_level(A1);
+    cups += check_level(A2);
+    cups += check_level(A3);
+    cups += check_level(A4);
+    cups += check_level(A5);
+    cups += check_level(A6);
+    cups += check_level(A7);
 
     // return the amount of cups
     return cups;
@@ -65,7 +70,7 @@ int measureCups() {
 }
 
 // check variance of polling
-int checkFloater(int pin) {
+int check_level(int pin) {
 
     int test = 0;
     int level = 0;
@@ -82,104 +87,98 @@ int checkFloater(int pin) {
 }
 
 // check for timeout conditions,after 15 seconds
-void checkTimeout() {
+void check_time() {
 
     // get the current time
     unsigned long now = millis();
 
-    // only perform the check every 10s
-    if( now-tempTime>10000UL ){
-        // if cups are empty, countdown timer
-        if ( measureCups() == 0 && !shuttingDown ) {
-            finishTime = millis();
-            shuttingDown = 1;
-        }
+    // (check every 10s) & cups are empty, start countdown timer
+    if( now-temp_time>10000UL && measure_cups() == 0 && !shutting_down ) {
+        finish_time = millis();
+        shutting_down = 1;
     }
 
     // wait 300 seconds after finishing brewing
-    if( now-finishTime>300000UL && shuttingDown ){
-        sprintf(publishString,"{\"BrewStatus\": \"done\"}",cups);
-        Spark.publish("BrewDone",publishString);
-        changeState("local");
-        shuttingDown = 0;
+    if( now-finish_time>300000UL && shutting_down ){
+        power_off(); // actually turn off machine
+        sprintf(publish_string,"{\"Brew_status\": \"done\"}",cups);
+        Spark.publish("Brew_done",publish_string);
+        shutting_down = 0;
     }
 
 }
 
 // LCD Functions:
 // check for timeout conditions for LCD
-void checkLCDTimeout() {
+void check_lcd() {
     unsigned long now = millis();
-    if (now-lcdTime>5000UL && lcdStatus == 0){
-        lcdStatus = 1;
-        displayCups();
-    }
-    else if (now-lcdTime>5000UL && lcdStatus == 1){
-        lcdStatus = 2;
-        displaySlogan();
-    }
-    else if (now-lcdTime>5000UL && lcdStatus == 2){
-        lcdStatus = 0;
-        displayWelcome();
+    if (now-lcd_time>5000UL && lcd_status == 0){
+        lcd_status = 1;
+        display_cups();
+    } else if (now-lcd_time>5000UL && lcd_status == 1){
+        lcd_status = 2;
+        display_slogan();
+    } else if (now-lcd_time>5000UL && lcd_status == 2){
+        lcd_status = 0;
+        display_welcome();
     }
 }
 
-void selectLineOne(){  //puts the cursor at line 0 char 0.
+void select_line_one(){  //puts the cursor at line 0 char 0.
     Serial.write(0xFE); //command flag
     Serial.write(128);  //position
     delay(40);
 }
-void selectLineTwo(){  //puts the cursor at line 0 char 0.
+
+void select_line_two(){  //puts the cursor at line 1 char 0.
     Serial.write(0xFE); //command flag
     Serial.write(192);  //position
     delay(40);
 }
 
-// display Mr. Coffi welcome message on LCD - status=0
-void displayWelcome(){
+void prepare_serial(){
     Serial1.write(0xFE); // command flag
     Serial1.write(0x01); // clear display
-    selectLineOne();
-    Serial1.write(" Welcome to the");
-    Serial1.write("        Mr.Cof-Fi!");
-    lcdTime = millis();
+    select_line_one();    // call above method
 }
 
-// read number of cups in tnak and display on LCD - status=1
-void displayCups(){
-    Serial1.write(0xFE); // command flag
-    Serial1.write(0x01); // clear display
-    selectLineOne();
+// display Mr. Coffi welcome message on LCD - status=0
+void display_welcome(){
+    prepare_serial();
+    Serial1.write(" Welcome to the");
+    Serial1.write("        Mr.Cof-Fi!");
+    lcd_time = millis();
+}
+
+// read number of cups in tank and display on LCD - status=1
+void display_cups(){
+    prepare_serial();
     Serial1.write("Cups in tank: ");
-    Serial1.print(measureCups(), HEX);
-    lcdTime = millis();
+    Serial1.print(measure_cups(), HEX);
+    lcd_time = millis();
 }
 
 // display slogan on LCD - status=2
-void displaySlogan(){
-    Serial1.write(0xFE); // command flag
-    Serial1.write(0x01); // clear display
-    selectLineOne();
+void display_slogan(){
+    prepare_serial();
     Serial1.write("     Yo for");
-    selectLineTwo();
+    select_line_two();
     Serial1.write("             some jo!");
-    lcdTime = millis();
+    lcd_time = millis();
 }
 
 // display brewing on LCD until done brewing
-void displayBrewing(){
-    Serial1.write(0xFE); // command flag
-    Serial1.write(0x01); // clear display
-    selectLineOne();
+void display_brewing(){
+    prepare_serial();
     Serial1.write("Brewing...");
-    selectLineTwo();
+    select_line_two();
     Serial1.write("             some jo!");
 }
 
 void setup() {
 
     // spark core definitions
-    Spark.function("swap", changeState);
+    Spark.function("brew", brew);
 
     // pin mode definitions
     pinMode(mrcf, OUTPUT);
@@ -199,13 +198,12 @@ void setup() {
     // Serial LCD setup
     Serial1.begin(9600);
     delay(20);
-    displayWelcome();
+    display_welcome();
+
 }
 
 void loop(){
     // check brew status for timeout
-    if( brewing ) checkTimeout();
-    else checkLCDTimeout();
+    if( brewing ) check_time();
+    else check_lcd();
 }
-
-
